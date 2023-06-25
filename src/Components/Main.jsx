5322fec9042b95bcc734a2e9b082acf7671eced7
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-06-25 11:33:24 -0600

    Commit changes 2023-06-25

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Main() {
     return (
-        <main className="main-card">
-            <img src="..src/assets/React_logo.png" alt="" />
+        <main className="main--card">
+            <img src="../assets/React_logo.png" alt="" />
             <h1 className="main--title">Fun facts about React</h1>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Navbar() {
     return (
-        <nav>
-            <img src="..assets/React_logo.png" className="nav--icon" />
+        <nav className="nav--container">
+            <img src="../React_logo.png" className="nav--icon" alt="React logo"/>
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
