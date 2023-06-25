859e7d61d37ea386e6a12bdf157f0db6ee9079db
Mohamed <mohamedmostafakhudari@gmail.com>
2023-06-25 23:17:45 +0300

    edit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-
+import imgUrl from "../assets/images/React_logo.png";
 export default function Navbar() {
   return (
     <nav className="nav--container">
-      <img
-        src="src/assets/images/React_logo.png"
-        className="nav--icon"
-        alt="React logo"
-      />
+      <img src={imgUrl} className="nav--icon" alt="React logo" />
       <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
