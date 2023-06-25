ac51bfcb7fc2c059193118952494760a84e6546f
Mohamed <mohamedmostafakhudari@gmail.com>
2023-06-25 22:58:38 +0300

    edit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from "react"
+import React from "react";
 
 export default function Navbar() {
-    return (
-        <nav className="nav--container">
-            <img src="../React_logo.png" className="nav--icon" alt="React logo"/>
-            <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - Project 1</h4>
-        </nav>
-    )
+  return (
+    <nav className="nav--container">
+      <img
+        src="src/assets/images/React_logo.png"
+        className="nav--icon"
+        alt="React logo"
+      />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
 }
